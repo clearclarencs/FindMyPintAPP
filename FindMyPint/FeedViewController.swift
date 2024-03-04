e5a9ebb8e0eb403daee909f7a93691729fbefdb2
clearclarencs <53274728+clearclarencs@users.noreply.github.com>
2024-03-04 13:35:42 +0000

    Map fixes, constraints for diff formats

--- FindMyPint/FeedViewController.swift
+++ FindMyPint/FeedViewController.swift
@@ -7,23 +7,83 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var posts = [
+        ("Hopey344", "The cocktail selection was fantastic @Sphinx #Bargain"),
+        ("GuinessLover82", "At the @brookhouse watching the Derby game, great game."),
+        ("DawnB1945", "£7 rosé at @Flute #WineDrunk #Tipsy #Lads"),
+        ("TheSphinx 🍺", "£2.20 Carling all week long, who could say no 🥵"),
+        ("MrLeahy", "Rated @Hope&Anchor ★★★★✩"),
+        ("EWilton69", "@Popworld WKD is lethal, had 2 last night blacked out and woke up in a police cell this morning. #FridayNights #WildOne #LGBT"),
+        ("Hatch 🍺", "Free entry all night, blasting y2k club anthems. Roll on the weekend."),
+        ("TheBlackCat 🍺", "Quiz night Wednesday‼️ £20 up for grabs but doors close 6pm."),
+        ("Hopey344", "The cocktail selection was fantastic @Sphinx #Bargain"),
+        ("GuinessLover82", "At the @brookhouse watching the Derby game, great game."),
+        ("DawnB1945", "£7 rosé at @Flute #WineDrunk #Tipsy #Lads"),
+        ("TheSphinx 🍺", "£2.20 Carling all week long, who could say no 🥵"),
+        ("MrLeahy", "Rated @Hope&Anchor ★★★★✩"),
+        ("EWilton69", "@Popworld WKD is lethal, had 2 last night blacked out and woke up in a police cell this morning. #FridayNights #WildOne #LGBT"),
+        ("Hatch 🍺", "Free entry all night, blasting y2k club anthems. Roll on the weekend."),
+        ("TheBlackCat 🍺", "Quiz night Wednesday‼️ £20 up for grabs but doors close 6pm."),
+        ("Hopey344", "The cocktail selection was fantastic @Sphinx #Bargain"),
+        ("GuinessLover82", "At the @brookhouse watching the Derby game, great game."),
+        ("DawnB1945", "£7 rosé at @Flute #WineDrunk #Tipsy #Lads"),
+        ("TheSphinx 🍺", "£2.20 Carling all week long, who could say no 🥵"),
+        ("MrLeahy", "Rated @Hope&Anchor ★★★★✩"),
+        ("EWilton69", "@Popworld WKD is lethal, had 2 last night blacked out and woke up in a police cell this morning. #FridayNights #WildOne #LGBT"),
+        ("Hatch 🍺", "Free entry all night, blasting y2k club anthems. Roll on the weekend."),
+        ("TheBlackCat 🍺", "Quiz night Wednesday‼️ £20 up for grabs but doors close 6pm."),
+
+            
+    ]
+    let blueTheme = UIColor(red: 60.0/255.0, green: 133.0/255.0, blue: 168.0/255.0, alpha: 1.0)
+    let yellowTheme = UIColor(red: 252.0/255.0, green: 165.0/255.0, blue: 3.0/255.0, alpha: 1.0)
+    
+    func formatPost(_ author: String, _ comment: String) -> NSAttributedString {
+        let text = author+"\n"+comment
+        let authorRange = NSRange(location: 0, length: text.split(separator: "\n")[0].count+1)
+        let attributedString = NSMutableAttributedString(string: text, attributes: [.foregroundColor: UIColor.label])
+        
+        let tagRegex = "(?:\\#[\\w&]+)|(?:\\@[\\w&]+)" // Regular expression to match #hashtags and @mentions
+        
+        do {
+            let regex = try NSRegularExpression(pattern: tagRegex, options: [])
+            let matches = regex.matches(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count))
+            
+            for match in matches {
+                attributedString.addAttribute(.foregroundColor, value: blueTheme, range: match.range)
+            }
+        } catch {
+            print("Regex error: \(error)")
+        }
+        
+        // Set author attributes
+        attributedString.addAttribute(.foregroundColor, value: yellowTheme, range: authorRange)
+        attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 14), range: authorRange)
+        
+        return attributedString
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let aCell = tableView.dequeueReusableCell(withIdentifier: "aPost", for: indexPath)
+        var content = UIListContentConfiguration.cell()
+        content.attributedText = formatPost("@"+posts[indexPath.row].0, posts[indexPath.row].1)
+//        content.secondaryText = posts[indexPath.row].1
+        content.textProperties.font = UIFont.systemFont(ofSize: 20)
+//        content.secondaryTextProperties.font = UIFont.systemFont(ofSize: 20)
+        aCell.contentConfiguration = content
+        
+        return aCell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- FindMyPint/MapViewController.swift
+++ FindMyPint/MapViewController.swift
@@ -1,29 +1,90 @@
 //
-//  MapViewController.swift
-//  FindMyPint
+//  ViewController.swift
+//  UserLocation
 //
-//  Created by James Skipworth on 03/03/2024.
+//  Created by Phil Jimmieson on 20/11/2022.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    let pubs = [
+        ("The Sphinx", 53.40501748448972, -2.966496069757191),
+        ("The Augustus John", 53.405754859909464, -2.964630449373654),
+        ("The Font", 53.40303429268724, -2.9685573),
+        ("The Bullring Bar", 53.40880117625418, -2.9708216908620537),
+        ("Hope & Anchor", 53.402379633989376, -2.9709933522383745),
+        ("The Cambridge Public House", 53.4024349071364, -2.9667896)
+    ]
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    var firstRun = true
+    var startTrackingTheUser = false
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locationOfUser = locations[0] //this method returns an array of locations
+        
+        let latitude = locationOfUser.coordinate.latitude
+        let longitude = locationOfUser.coordinate.longitude
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        if firstRun {
+            firstRun = false
+            let latDelta: CLLocationDegrees = 0.01
+            let lonDelta: CLLocationDegrees = 0.01
+            let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+            
+            //a region defines a centre and a size of area covered.
+            let region = MKCoordinateRegion(center: location, span: span)
+            
+            //make the map show that region we just defined.
+            self.mapView.setRegion(region, animated: true)
+            
+            _ = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(startUserTracking), userInfo: nil, repeats: false)
+        }
+        
+        if startTrackingTheUser == true {
+            mapView.setCenter(location, animated: true)
+        }
+        
+        // Plot local pubs
+        for (pub, lat, long) in pubs{
+            let newCoordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinate
+            annotation.title = pub
+            self.mapView.addAnnotation(annotation)
+        }
+    }
+    
+    //this method sets the startTrackingTheUser boolean class property to true. Once it's true, subsequent calls
+    //to didUpdateLocations will cause the map to center on the user's location.
+    @objc func startUserTracking() {
+        startTrackingTheUser = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self as CLLocationManagerDelegate
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
