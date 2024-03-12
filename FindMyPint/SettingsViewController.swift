c5f73e3b2a96d5e9d423ad62ffd68be9ef1c748c
clearclarencs <53274728+clearclarencs@users.noreply.github.com>
2024-03-12 15:33:25 +0000

    Username first login box, seperate settings view

---
+++ FindMyPint/SettingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsViewController.swift
+//  FindMyPint
+//
+//  Created by James Skipworth on 12/03/2024.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
