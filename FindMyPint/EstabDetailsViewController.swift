ba5e2ebcbfd7f4f1b6fcb22858352f41caa98907
clearclarencs <53274728+clearclarencs@users.noreply.github.com>
2024-05-03 17:40:57 +0100

    Fix plan for me

---
+++ FindMyPint/EstabDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EstabDetailsViewController.swift
+//  FindMyPint
+//
+//  Created by James Skipworth on 18/04/2024.
+//
+
+import UIKit
+
+class EstabDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FindMyPint/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  FindMyPint
+//
+//  Created by James Skipworth on 18/04/2024.
+//
+
+import Foundation
